4322a751fbc29f8ca3f208f4d93f3386cbedee88
Mr. Zok3r <96907156+k4long666@users.noreply.github.com>
2022-07-01 18:19:04 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,4 @@
 import dump
 dump.lisen_ku()
-if __name__=="__main__":
-    try:os.mkdir("DUMP")
-    except:pass
-    os.system('git pull')
-    os.system('clear');loading()
-    folderdata()
-    lisen_ku()
+try:os.mkdir("DUMP")
+except:pass
